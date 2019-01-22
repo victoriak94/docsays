0e5875b724eb7814a1c86da897cb1b10aedf949f
victoriak94 <vkamila6@gmail.com>
2019-01-22 17:07:57 -0500

    Create new plural controllers for doctors and patients. Add single patient view.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get('home/index')
   root('home#index')
 
+  resources :patients
+  resource :doctors
+  resource :mealplan
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
