8cd88c25186ee2394e2f975c7a334ac41d32248b
victoriak94 <vkamila6@gmail.com>
2019-01-22 15:40:00 -0500

    added controller methods

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -1,19 +1,31 @@
 class DoctorController < ApplicationController
 
   def show
-
+   @doctor = Doctor.find(params[:id])
   end
 
   def new
-
+    @doctor = Doctor.new
   end
 
   def create
+    @doctor = Doctor.new
+    @doctor.name = params[:doctors][:name]
+    @doctor.specialization = params[:doctors][:specialization]
+     if @restaurant.save
+      redirect_to doctors_path(@doctor)
+      flash[:notice] = "We added you!"
+    else
+      render :new
+      flash[:notice] = "We could not add you"
+    end
+
 
   end
 
   def edit
-
+    @doctor.name = params[:doctors][:name]
+    @doctor.address = params[:doctors][:address]
   end
 
   def update
--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,31 +1,53 @@
 class PatientController < ApplicationController
 
   def index
-
+    @patients = Patient.all
   end
 
   def show
-
+    @patient = Patient.find(params[:id])
   end
 
   def new
-
+    @patient = Patient.new
   end
 
   def create
+    @patient = Patient.new
+    @patient.name = params[:patient][:name]
+    @patient.sex = params[:patient][:sex]
+    @patient.age = params[:patient][:age]
 
+    if @patient.save
+      redirect_to patient_path(@patient)
+      flash[:notice] = "You have added your patient!"
+    else
+      render :new
+      flash[:notice] = "We could not add your patient"
+    end
   end
 
   def edit
-
   end
 
   def update
+    @patient.name = params[:patient][:name]
+    @patient.sex = params[:patient][:sex]
+    @patient.age = params[:patient][:age]
 
+    if @patient.save
+      redirect_to patient_path(@patient)
+      flash[:notice] = "You have added your patient!"
+    else
+      render :new
+      flash[:notice] = "We could not add your patient"
+    end
   end
 
   def destroy
-
+    @patient.destroy
+    redirect_to "/patients"
+    flash[:notice] = "Patient deleted"
   end
 
 end
