7b6ff790357d0b486db3c64fb099b3fb8437f169
victoriak94 <vkamila6@gmail.com>
2019-01-23 13:28:43 -0500

    Update landing/home page. Generate devise sessions and registrations controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  before_action(:authenticate_patient!)
-  before_action(:authenticate_doctor!)
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :doctors
+  devise_for :doctors, controllers: {
+    sessions: 'doctors/sessions',
+    registrations: 'doctors/registrations' }
   devise_for :patients
 
   get('home/index')
   root('home#index')
 
   resources :patients
-  resource :doctors
+  resources :doctors
   resource :mealplan
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
