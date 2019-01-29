5ce765ef576bd89cb780fc9939eb99c3da9841ae
victoriak94 <vkamila6@gmail.com>
2019-01-29 12:01:22 -0500

    Remove pg_search_scope from diet model

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -2,13 +2,5 @@ class Diet < ApplicationRecord
   belongs_to :diagnosis
   has_one(:patient, {:through => :diagnosis})
 
-  pg_search_scope :search_by_term, against: [:title, :content],
-  using: {
-    tsearch: {
-      any_word: true,
-      prefix: true
-    }
-  }
-
   validates :name, presence: true
 end
