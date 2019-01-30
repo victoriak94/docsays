2a520599f2256acd6fef6dd7c32dcf2d94bc3ece
victoriak94 <vkamila6@gmail.com>
2019-01-30 17:25:08 -0500

    Update rake task to see if that fixes heroku deployment error

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,7 +5,7 @@ class Patient < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :diagnoses
-  has_one :meal_plan
+
   has_attached_file :image, styles: {thumb: "150x150#"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   extend Devise::Models
