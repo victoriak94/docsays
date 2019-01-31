80e5b03833c8bdd14f8b05e9294e7ba86890b8d1
victoriak94 <vkamila6@gmail.com>
2019-01-30 19:31:33 -0500

    Change env variables for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: Figaro.env.MAIL_HOST! }
+  config.action_mailer.default_url_options = { host: Figaro.env.APP_HOST! }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      Figaro.env.SENDMAIL_USERNAME!,
