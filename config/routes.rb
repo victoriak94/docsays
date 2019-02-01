a50b25d4af5bbaa723d14513cab31f60ed4a220f
msynko <marina.synko@gmail.com>
2019-01-31 19:55:52 -0500

    gridd css to display patients

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,5 @@ Rails.application.routes.draw do
   post '/searchpatient' => 'patients#search'
 end
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
