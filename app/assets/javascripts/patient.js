6b1a8857c142350eba73d8b6a0da33c9d21be9f7
victoriak94 <vkamila6@gmail.com>
2019-02-12 00:00:00 -0500

    Modify search function

--- app/assets/javascripts/patient.js
+++ app/assets/javascripts/patient.js
@@ -1,52 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-//  axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-// const myForm = document.querySelector('.patient-search');
-
-// myForm.addEventListener('submit', function(e){
-//     e.preventDefault();
-//     console.log("I m in the event submit");
-//     const formData = new FormData(myForm);
-//     axios({
-//         method: myForm.getAttribute('method'),
-//         url: myForm.getAttribute('action'),
-//         data: formData,
-//         headers:{
-//             'Content-Type': 'application/html',
-//             'Accept': 'text/html'
-//         }
-//        // responseType: 'JSON'
-//     }).then(function(response){
-
-//         console.log("Success"+response.data);
-//         document.querySelector('#patient_name').value = '';
-//        const myDiv = document.querySelector('#searchResult');
-//        //
-//        //  // const newItem = document.createElement('li');
-//        //  // newItem.innerText = response.data.content;
-//        myDiv.innerHTML = '';
-//        myDiv.insertAdjacentHTML('afterbegin',response.data);
-//       //myDiv.appendChild(response.data);
-
-
-//     });
-
-// })
-
 let filterInput = document.querySelector("#filterInput");
-filterInput.addEventListener("keyup", filterNames);
+filterInput.addEventListener('keyup', filterNames);
 
 function filterNames(){
   let filterValue = document.querySelector("#filterInput").value.toUpperCase();
   let li = document.querySelectorAll("li.patient");
 
   for(let i = 0; i < li.length; i++){
-    let a = li[i].getElementsByTagName('a')[0];
+    let a = li[i].querySelectorAll('a')[0];
     if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
       li[i].style.display = '';
     } else {
       li[i].style.display = 'none';
     }
   }
-
+} 
 });
