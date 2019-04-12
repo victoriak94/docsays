8fa9a82df3f19dae616f0e92ab32ce8eed622482
victoriak94 <vkamila6@gmail.com>
2019-04-12 15:02:26 -0400

    Fix patient search filter bug. Make form text colors white

--- app/assets/javascripts/patient.js
+++ app/assets/javascripts/patient.js
@@ -1,18 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-let filterInput = document.querySelector("#filterInput");
-filterInput.addEventListener('keyup', filterNames);
+document.addEventListener("DOMContentLoaded", () => {
+  let filterInput = document.querySelector("#filterInput");
+  filterInput.addEventListener("keyup", filterNames);
 
-function filterNames(){
-  let filterValue = document.querySelector("#filterInput").value.toUpperCase();
-  let li = document.querySelectorAll("li.patient");
+  function filterNames() {
+    let filterValue = document
+      .querySelector("#filterInput")
+      .value.toUpperCase();
+    let li = document.querySelectorAll("li.patient");
 
-  for(let i = 0; i < li.length; i++){
-    let a = li[i].querySelectorAll('a')[0];
-    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
-      li[i].style.display = '';
-    } else {
-      li[i].style.display = 'none';
+    for (let i = 0; i < li.length; i++) {
+      let a = li[i].querySelectorAll("#patientName")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
     }
   }
-} 
 });
